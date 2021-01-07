987fd0d51ac9caf1bea5bc64d4ec904ee33fe862
EliaDassio <71812349+EliaDassio@users.noreply.github.com>
2021-01-07 09:48:52 +0100

    Update es_2.py

--- PYTHON/es_vacanze/es_2/es_2.py
+++ PYTHON/es_vacanze/es_2/es_2.py
@@ -2,21 +2,21 @@ import random
 
 def genera_mac():
     for i in range(6):
-        des = random.randint(0, 3)
+        des = random.randint(0, 1)
         if des == 1:
             val_1 = random.randint(48, 57)
         else:
             val_1 = random.randint(65, 70)
-        des = random.randint(0, 3)
+        des = random.randint(0, 1)
         if des == 1:
             val_2 = random.randint(48, 57)
         else:
             val_2 = random.randint(65, 70)
-        if i < 6:
+        if i <= 4:
             print(chr(val_1) + chr(val_2) + ":", end = '')
         else:
             print(chr(val_1) + chr(val_2))
-    print("\n")
+    print("")
 
 
 ready = input("generate a mac address ?(press enter): ")
